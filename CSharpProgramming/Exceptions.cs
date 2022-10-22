2bb51edfa72e0a9a53a28e4cc2d2b1c4870158d8
Bindu <bindurani9843@gmail.com>
2022-10-22 17:11:54 +0530

    Add project files.

---
+++ CSharpProgramming/Exceptions.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpProgramming
+{
+    internal class Exceptions
+    {
+        static void Main()
+        {
+
+            // This block raises an exception
+            try
+            {
+
+                byte data = byte.Parse("a");
+                Console.WriteLine(data);
+            }
+
+            // Catch block 1
+
+            // This block is used to handle
+            // IndexOutOfRangeException type exception
+            catch (IndexOutOfRangeException)
+            {
+
+                Console.WriteLine("At least provide one Argument!");
+            }
+
+            // Catch block 2
+
+            // This block is used to handle
+            // FormatException type exception
+            catch (FormatException)
+            {
+
+                Console.WriteLine("Entered value in not a number!");
+            }
+
+            // Catch block 3
+
+            // This block is used to handle
+            // OverflowException type exception
+            catch (OverflowException)
+            {
+
+                Console.WriteLine("Data is out of Range!");
+            }
+        }
+    }
+}
---
+++ CSharpProgramming/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpProgramming
+{
+    /// <summary>
+    /// Inheritance-Code Reusability
+    /// Types-Single,Multi-level,Hybrid,Hierarchial
+    /// Multiple Inheritance-Not supported-instead Interfaces are used
+    /// protected access specifier
+    /// </summary>
+    /// BASE CLASS 
+    internal class Employee
+    {
+        protected int EID;
+        protected string EName;
+        protected int TDID;
+        protected string TDName;
+        protected void Display()
+        {
+            Console.WriteLine("BASE CLASS");
+        }
+    }
+    /// <summary>
+    /// Child Class - Department
+    /// Single-level
+    /// </summary>
+    internal class Department : Employee
+    {
+        protected int DID;
+        protected string DName;
+
+    }
+    /// <summary>
+    /// Hierarchial Inheritance
+    /// </summary>
+    internal class TrainingDepartment : Employee
+    {
+        public void GetTDetails()
+        {
+            base.Display();
+            TDID = 101;
+            TDName = "DOTNET";
+            Console.WriteLine($"TDID is {TDID} and TDName is {TDName}");
+        }
+    }
+    internal class Inheritance
+    {
+        public static void Main()
+        {
+            //Single-level
+            // Department department = new Department();
+            // department.GetEmployeeDetails();
+            // department.Display();
+
+            //Multi-level
+
+
+            //Hierarchial
+            TrainingDepartment trainingDepartment = new TrainingDepartment();
+            trainingDepartment.GetTDetails();
+
+        }
+    }
+}
+    
